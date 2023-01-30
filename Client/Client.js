9cbd2758da0ee0adfc3572f6410c17d647213c64
Esteban <98122124+EpsyUwU@users.noreply.github.com>
2023-01-29 18:26:26 -0600

    sera que ya quedo

--- Client/Client.js
+++ Client/Client.js
@@ -1,22 +1,40 @@
-const net = require('net')
+
 const readline = require('readline-sync')
 
+
+let name = readline.question("Cual es tu nombre?> ")
+let stdin = process.openStdin()
+
+const net = require('net')
 const options = {
     port: 4000,
     host: '127.0.0.1' 
 }
 
-const client = net.createConnection(options);
+const client = net.createConnection(options, () =>{
 
-client.on('connect', () => {
-    console.log('conexion exitosa')
-    sentLine()
+    
+    console.log("Bienvenido al chat " + name)
+    client.write("userName"+name)
+
+    
+    
+    client.on('data', (data) => {//recibe el mensaje
+        process.stdout.write("\n" + data)
+        process.stdout.write("\nyo>")
+
+        if(data == "nombre ya usado"){
+            let name = readline.question("Cual es tu nombre?> ")
+            client.write("userName"+name)
+        }
+    });
+ 
+    stdin.addListener('data', (data) => {//mando el mensaje
+        process.stdout.write("yo>")
+        client.write(name + ">" + data)
+    })
 })
 
-client.on('data',(data) =>{
-    console.log('recibes del server: ' + data.toString())
-    sentLine()
-})
 
 client.on('error', (error) =>{
     console.log(error.message)
@@ -25,14 +43,3 @@ client.on('error', (error) =>{
 client.on('end',()=>{
     console.log('ayoooooo usuario')
 })
-
-function sentLine(){
-
-    let line = readline.question('\nescribe algo: \t')
-    
-    if(line =='0'){
-        client.end()
-    }else{
-        client.write(line)
-    }
-}
